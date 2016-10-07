c042860913fcc3c62fc4db4cf99e9f6e48f83d1c
doneill612 <doneill612@gmail.com>
2016-10-07 23:31:03 +0200

    Post-class updates, improved structure

--- TP2/metronome.f95
+++ TP2/metronome.f95
@@ -1,41 +1,46 @@
 module functionConstants
 implicit none
-real :: g = 9.81, l = 0.20, m = 0.0250, Co = 1.0, nu = 1.3, pi = atan(-1.0)
+real :: g = 9.81, l = 0.20, m1 = 0.100, m2 = 0.025, nu = 1.3, Co = 1.0, pi = atan(-1.0)
 end module
 
 program Metronome
 implicit none
 external :: solve
-call solve(100.0)
+call solve(10, .false.)
+call solve(1000, .true.)
 end program
 
-subroutine solve(tau)
+subroutine solve(tMax, dampened)
 implicit none
 external :: writeToFile
-integer :: i
-real :: tau, t, dt, r, a, next, derivative, potentialEnergy, kineticEnergy
+logical :: dampened
+integer :: tMax, t
+real :: a, b, r, dt, nextDampened, next, derivative, potential, kinetic
 real, dimension(5) :: theta
 real, dimension(3) :: energy
-t = 0.0
 dt = 0.01
-a = 0.1
-r = dt / (2 * tau)
-!-- theta = [ q(t+dt), q(t), q(t-dt), dq/dt, t ] --!
 theta = (/ 0.0, 2.5, 2.5, 0.0, 0.0 /)
-energy = (/ kineticEnergy(theta) , potentialEnergy(theta), potentialEnergy(theta) + kineticEnergy(theta) /) 
-open(unit = 7, file = "MetronomeDampened.out", access = "append", action = "write")
-open(unit = 8, file = "EnergyDampened.out", access = "append", action = "write")
-do i =1,100000
-    t = i * dt
-    theta(5) = t
-    theta(1) = next(theta, dt, a, r)
-    theta(4) = derivative(theta, dt)
-    energy(1) = kineticEnergy(theta)
-    energy(2) = potentialEnergy(theta)
-    energy(3) = energy(1) + energy(2)
-    call writeToFile(theta, energy)
-    theta(2:3) = theta(1:2)
-    t = t + dt
+energy = (/ kinetic(theta, dampened), potential(theta, dampened), sum((/kinetic(theta, dampened), potential(theta, dampened) /)) /)
+if(dampened) then
+	open(unit = 7, file = "MetronomeDampened.out", access = "append", action = "write")
+	open(unit = 8, file = "EnergyDampened.out", access = "append", action = "write")
+else
+	open(unit = 7, file = "MetronomeUndampened.out", access = "append", action = "write")
+	open(unit = 8, file = "EnergyUndampened.out", access = "append", action = "write")
+endif
+do t = 0, tMax * 100
+	theta(5) = t * dt
+	if(dampened .eqv. .true.) then
+		theta(1) = nextDampened(theta, dt)
+	else
+		theta(1) = next(theta, dt)
+	endif
+	theta(4) = derivative(theta, dt)
+	energy(1) = kinetic(theta, dampened)
+	energy(2) = potential(theta, dampened)
+	energy(3) = sum((/energy(1), energy(2) /))
+	call writeToFile(theta, energy)
+	theta(2:3) = theta(1:2)
 enddo
 close(7)
 close(8)
@@ -49,20 +54,6 @@ write(7, *) theta(5), theta(2), theta(4)
 write(8, *) energy(1), energy(2), energy(3), theta(5)
 end subroutine
 
-real function potentialEnergy(theta)
-use functionConstants
-implicit none
-real, dimension(5) :: theta
-potentialEnergy = m * g * l * cos(theta(2)) + (1.0/2.0)*Co*(l*theta(2))**2.0
-end function
-
-real function kineticEnergy(theta)
-use functionConstants
-implicit none
-real, dimension(5) :: theta
-kineticEnergy = (1.0/2.0) * m * (l*theta(4))**2.0
-end function
-
 real function derivative(theta, dt)
 implicit none
 real :: dt
@@ -70,10 +61,49 @@ real, dimension(5) :: theta
 derivative = (theta(1) - theta(3)) / (2.0 * dt)
 end function
 
-real function next(theta, dt, a, r)
+real function kinetic(theta, dampened)
 use functionConstants
 implicit none
-real :: dt, a, r
+logical :: dampened
+real :: soln
 real, dimension(5) :: theta
-next = (2.0*theta(2)+(r-1.0)*theta(3)+((dt**2.0)*((g/l)*sin(theta(2))-((Co/m)*theta(2)) + a * sin(2.0*pi*nu*theta(5))))) / (r+1.0)
+if(dampened .eqv. .true.) then
+    soln = (1.0/2.0) * m2 * (l*theta(4))**2.0
+else
+    soln = (1.0/2.0) * m1 * (l*theta(4))**2.0
+endif
+kinetic = soln
+end function
+
+real function potential(theta, dampened)
+use functionConstants
+implicit none
+logical :: dampened
+real :: soln
+real, dimension(5) :: theta
+if(dampened .eqv. .true.) then
+    soln = m2 * g * l * cos(theta(2)) + (1.0/2.0)*Co*(l*theta(2))**2.0
+else
+    soln = m1 * g * l * cos(theta(2)) + (1.0/2.0)*Co*(l*theta(2))**2.0
+endif
+potential = soln
+end function
+
+real function next(theta, dt)
+use functionConstants
+implicit none
+real :: dt
+real, dimension(5) :: theta
+next = (2.0*theta(2)-theta(3)+((dt**2.0)*((g/l)*sin(theta(2))-((Co/m1)*theta(2))+ 0.0*sin(2.0*pi*nu*theta(5)))))
+end function
+
+real function nextDampened(theta, dt)
+use functionConstants
+implicit none
+real :: dt, r, a, tau
+real, dimension(5) :: theta
+a = 0.5
+tau = 100.0
+r = dt / (2.0 * tau)
+nextDampened=(2.0*theta(2)+(r-1.0)*theta(3)+((dt**2.0)*((g/l)*sin(theta(2))-((Co/m2)*theta(2))+a*sin(2.0*pi*nu*theta(5)))))/(r+1.0)
 end function
